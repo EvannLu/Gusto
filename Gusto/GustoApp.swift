fb9d2461f2b95ce007ce0f0989f23e5bf38d05e8
EvannLu <evan.lu60@myhunter.cuny.edu>
2025-11-17 14:41:23 -0500

    Initial Commit

---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by Evan Lu on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
